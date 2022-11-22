81074e5e423a71396a9ad5a50a4ad99ff9565696
josandg@net.chalmers.se <gussanjoba@student.gu.se>
2022-11-22 17:14:10 +0100

    food & point starter

--- src/main/java/com/example/snakegame/Food.java
+++ src/main/java/com/example/snakegame/Food.java
@@ -1,5 +1,27 @@
 package com.example.snakegame;
 
 public class Food {
+    private Point location;
 
+    public Food(Point location) {
+        this.location = location;
+    }
+
+    public Point getLocation() {
+        return location;
+    }
+
+    public void setLocation(Point location) {
+        this.location = location;
+    }
+
+    // 2 foods are the same if they are on the same position
+    public boolean equals(Object other) {
+        if (other instanceof Food) {
+            Food otherFood = (Food) other;
+            return location.equals(otherFood.location);
+        } else {
+            return false;
+        }
+    }
 }
