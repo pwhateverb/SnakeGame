59ff6d4734f80027b680d07a0778dd063ed78dc0
Alexander <helsinga@student.chalmers.se>
2022-11-22 17:24:26 +0100

    Game & Multiplayer

--- src/main/java/com/example/snakegame/Game.java
+++ src/main/java/com/example/snakegame/Game.java
@@ -1,4 +1,10 @@
 package com.example.snakegame;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Game {
+
+
 }
--- src/main/java/com/example/snakegame/Multiplayer.java
+++ src/main/java/com/example/snakegame/Multiplayer.java
@@ -1,4 +1,9 @@
 package com.example.snakegame;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class Multiplayer extends Game {
 }
