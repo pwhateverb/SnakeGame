98f3c7ed96ab87cc7a40964ea8f1c24019ffa935
josandg@net.chalmers.se <gussanjoba@student.gu.se>
2022-11-23 22:07:54 +0100

    food uses rectangle javafx object, removed point

--- src/main/java/com/example/snakegame/Food.java
+++ src/main/java/com/example/snakegame/Food.java
@@ -1,27 +1,37 @@
 package com.example.snakegame;
 
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+import java.util.Random;
+
 public class Food {
-    private Point location;
 
-    public Food(Point location) {
-        this.location = location;
+    // creates food Rectangle scene object, not sure which size we are going with
+    // tho!
+    private Rectangle food = new Rectangle(10, 10, Color.RED);
+
+    // first food is specified in main
+    public Food(int x, int y) {
+        food.setX(x);
+        food.setY(y);
     }
 
-    public Point getLocation() {
-        return location;
+    // generates a random food on the "grid", just call this when the snake eats
+    public void generateFood() {
+        Random rand = new Random();
+        int x = rand.nextInt(40) * 10;
+        int y = rand.nextInt(40) * 10;
+        food.setX(x);
+        food.setY(y);
     }
 
-    public void setLocation(Point location) {
-        this.location = location;
+    // get food x value
+    public double getFoodLocationX() {
+        return food.getX();
     }
 
-    // 2 foods are the same if they are on the same position
-    public boolean equals(Object other) {
-        if (other instanceof Food) {
-            Food otherFood = (Food) other;
-            return location.equals(otherFood.location);
-        } else {
-            return false;
-        }
+    // get food y value
+    public double getFoodLocationY() {
+        return food.getY();
     }
 }
