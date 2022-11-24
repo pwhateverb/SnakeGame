c5a064745e9f71fe81d50b0c70eb0631742166ba
David <davidroc@net.chalmers.se>
2022-11-24 11:37:47 +0100

    Highscore interface minor improvements

--- src/main/java/com/example/snakegame/IHighscore.java
+++ src/main/java/com/example/snakegame/IHighscore.java
@@ -8,14 +8,14 @@ public interface IHighscore {
 
     /** returns true if user achieved a highScore. If so, the gameOver scene should display a prompt like "Input your name to be added to the highscores" */
     boolean highscoreCheck(int score);
-    //todo: call in the gameOver screen (when user crashes the snake)
+    //todo: call this method in the gameOver scene (when checking whether to display the insertNameForAchievement prompt)
 
     /** saves user's name and score in a file called "highscores.txt". */
     void saveHighscore(int score, String name);
-    //todo: call when the user inputs a name after being prompted in the gameOver scene
+    //todo: call this method when the user inputs a name (after being prompted to do so in the gameOver scene)
 
     /** returns a Scene displaying the 10 highest scores currently saved in a file called "highscores.txt". */
     Scene displayHighscores();
-    //todo: call in the menu, when user selects "See Highscores".
+    //todo: call this method when user selects the 'HIGH SCORES' menu option, in the Menu scene.
 
 }
