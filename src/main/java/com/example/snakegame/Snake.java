96a4ea3fda1f11824d95a8c7cafb8f9e845559f3
parisab <parisab@student.chalmers.se>
2022-11-24 12:24:49 +0100

    Snake first draft + Point

---
+++ src/main/java/com/example/snakegame/Snake.java
@@ -0,0 +1,62 @@
+package com.example.snakegame;
+
+import java.util.ArrayList;
+
+public class Snake {
+
+    private ArrayList<Point> bodyParts;
+    private Point headOfSnake;
+
+    public Snake(){
+        bodyParts = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            bodyParts.add(new Point(10, 10));
+        }
+
+        headOfSnake = bodyParts.get(0);
+    }
+
+    public void move(String direction){
+        int x = headOfSnake.getX();
+        int y = headOfSnake.getY();
+
+        switch (direction) {
+            case "right" : x++;
+                break;
+            case "left" : x--;
+                break;
+            case "up" : y++;
+                break;
+            case "down" : y--;
+                break;
+        }
+
+        bodyParts.add(0, new Point(x, y));
+        bodyParts.remove(bodyParts.size() - 1);
+        headOfSnake = bodyParts.get(0);
+    }
+
+    public void grow(){
+        bodyParts.add(new Point(0, 0));
+    }
+
+    public Point getHead(){
+        return headOfSnake;
+    }
+
+    public ArrayList<Point> getBody(){
+        return bodyParts;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+
+        Snake snake = (Snake) object;
+
+        if (!bodyParts.equals(snake.bodyParts)) return false;
+        return headOfSnake.equals(snake.headOfSnake);
+    }
+
+}
