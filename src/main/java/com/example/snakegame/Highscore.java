4ce61d915c9b67f850d4b0aa7fbddb8168abd1ca
David <davidroc@net.chalmers.se>
2022-11-29 11:48:06 +0100

    Highscore object (attributes: name and score)

---
+++ src/main/java/com/example/snakegame/Highscore.java
@@ -0,0 +1,28 @@
+package com.example.snakegame;
+
+public class Highscore implements Comparable<Highscore>{
+    final public String name;
+    final public int score;
+
+    public Highscore(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String toString() {
+        return name + " got " + score + " points.";
+    }
+
+    @Override
+    public int compareTo(Highscore otherHighscore) {
+        return otherHighscore.score -this.score;
+    }
+}
