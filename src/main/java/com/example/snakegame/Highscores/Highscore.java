1184da2341d4ef1caf7a00a16ed5afbccc752909
David <davidroc@net.chalmers.se>
2022-12-28 12:35:13 +0100

    Added documentation to Highscore.java

--- src/main/java/com/example/snakegame/Highscores/Highscore.java
+++ src/main/java/com/example/snakegame/Highscores/Highscore.java
@@ -1,5 +1,18 @@
 package com.example.snakegame.Highscores;
 
+
+/**
+ * Abstraction of a highscore object: it has the player's name and the respective numeric score. Can be written to a
+ * string or compared to other highscores.
+ * <p/>
+ * It does not have setters as <b>each object should only be created, not edited</b>.
+ * <p/>
+ * Used by the {@link com.example.snakegame.Highscores.ScoreEngine} class.
+ * <p/>
+ * <i>Can be refactored as a Record.</i>
+ * @see <a href="https://www.baeldung.com/java-record-keyword">Records</a>
+**/
+
 public class Highscore implements Comparable<Highscore>{
     final public String name;
     final public int score;
