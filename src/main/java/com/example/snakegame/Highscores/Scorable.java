605e822e2c265705f1faab24da6c76f26e3e59e6
David <davidroc@net.chalmers.se>
2022-12-28 12:36:30 +0100

    Added Scorable documentation

--- src/main/java/com/example/snakegame/Highscores/Scorable.java
+++ src/main/java/com/example/snakegame/Highscores/Scorable.java
@@ -5,30 +5,49 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * This interface connects the ScoreEngine to the rest of the game.
-    **/
+ * This interface uses the {@link com.example.snakegame.Highscores.ScoreEngine} class to provide post-game and
+ * highscore functionality to the game.
+**/
 
 public interface Scorable {
 
-    /** returns a Scene displaying the 10 highest scores currently saved in a file called "highscores.txt".
-     * Call this way, inside a onAction/onClick function:
+    /** This method returns a Scene displaying the 10 highest scores currently saved in a file called "highscores.txt".
+     * <p/>
+     * To be called inside a onAction/onClick function, with the following snippet:
+     *      <pre> {@code
      *      Scorable iHighscores = new ScoreEngine();
      *      Scene displayHighscoresScene = iHighscores.displayHighscores(stage, menuScene);
      *      stage.setScene(displayHighscoresScene);
-     * */
+     *      }</pre>
+     *
+     *  @param initialStage pass the JavaFX stage where the whole application / game is happening
+     *  @param initialScene pass a <b>new</b> JavaFX {@link com.example.snakegame.mainMenu} scene
+     *
+     *  @return returns a JavaFX scene that displays the top-10 highscores
+     *  @see <a href="https://docs.oracle.com/javase/8/javafx/api/javafx/scene/Scene.html">JavaFX Scene</a>
+    **/
+
     Scene displayHighscores(Stage initialStage, Scene initialScene);
 
 
-
     /**
-     * If user achieved a highScore, the gameOver scene displays a prompt like "Input your name to be added to the highscores"
-     * It saves user's name and score in a file called "highscores.txt".
-     * Call this way, inside a onAction/onClick function:
+     * This method returns a Scene displaying the post-game screen. If user achieved a highScore, a prompt to submit a
+     * highscore is included. A submission places the user's name and scoreAttained in a file called "highscores.txt".
+     * <p/>
+     * To be called inside a onAction/onClick function, with the following snippet:
+     *      <pre> {@code
      *      Scorable iHighscores = new ScoreEngine();
-     *      Scene postGameScreen = iHighscores.displayPostGame(stage, menuScene, randomNumber());
+     *      Scene postGameScreen = iHighscores.displayPostGame(stage, initialScene, scoreAttained);
      *      stage.setScene(postGameScreen);
-     * **/
-    Scene displayPostGame(Stage initialStage, Scene menuScene, int score);
-    //todo: create a new ScoreEngine object & call this method when user presses any key after losing the game, in the Game scene
+     *      }</pre>
+     *
+     *      @param initialStage pass the JavaFX stage where the whole application / game is happening
+     *      @param initialScene pass a <b>new</b> JavaFX {@link com.example.snakegame.mainMenu} scene
+     *      @param scoreAttained pass the score just attained by the user
+     *
+     *      @return returns a JavaFX scene that displays the post-game screen
+     *      @see <a href="https://docs.oracle.com/javase/8/javafx/api/javafx/scene/Scene.html">JavaFX Scene</a>
+    **/
 
+    Scene displayPostGame(Stage initialStage, Scene initialScene, int scoreAttained);
 }
