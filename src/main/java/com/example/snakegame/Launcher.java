8bfa1082ba738fa796d1b547b279e27a54d9582b
David <davidroc@net.chalmers.se>
2022-12-29 13:50:07 +0100

    Documented Launcher class

--- src/main/java/com/example/snakegame/Launcher.java
+++ src/main/java/com/example/snakegame/Launcher.java
@@ -1,5 +1,11 @@
 package com.example.snakegame;
 
+/**
+ * Launching class that does not inherit from (Java FX) Application class.
+ * <p/>
+ * Required to create .jar executable artifact.
+ */
+
 public class Launcher {
     public static void main(String[] args) {
         Menu.main(args);
